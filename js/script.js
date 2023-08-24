8eef80ebac367121526bfe918f46b82dc9e7c5a6
CaioHPerlin <caiohperlin@gmail.com>
2023-08-24 14:37:40 -0400

    add basic theme changing functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+let darkMode = true;
 
 const rainbow = [
     "#FFD3A3",
@@ -21,4 +23,13 @@ btn1.addEventListener('click', () => setInterval(() => {
     }
 }, 350));
 
-  
+let newBackgroundColor = "";
+let newPrimaryColor = "";
+btn2.addEventListener('click', () => {
+    newBackgroundColor = darkMode? "rgb(240,240,240)" : "rgb(29, 27, 31)";
+    darkMode = !darkMode;
+
+    document.body.style.backgroundColor = newBackgroundColor;
+    
+    btn2.innerText = `Modo ${darkMode? "Claro" : "Escuro"}`;
+});
