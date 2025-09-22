58ec585d846310ddc018bc6edd0ef93b55e8ca27
caiohperlin <caiohperlin@gmail.com>
2025-09-22 17:36:52 -0400

    feat: implement task creation logic

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.getElementById("addBtn").addEventListener("click", () => {
-  alert("Botão Adicionar clicado!");
+  const input = document.getElementById("task");
+  const texto = input.value.trim();
+  if (texto !== "") {
+    const li = document.createElement("li");
+    li.textContent = texto;
+    document.getElementById("list").appendChild(li);
+    input.value = "";
+  }
 });
